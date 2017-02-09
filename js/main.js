d77d89b4a833d6934be416c0b91d999720bf8276
dkammer <dietrich.kammer@visualistik.de>
2017-02-09 18:58:41 +0100

    JSON cleaning

--- js/main.js
+++ js/main.js
@@ -68,3 +68,9 @@ function drawFlowerplot(noOfRows) {
 
 
 drawFlowerplot(5);
+
+   var request = new XMLHttpRequest();
+   request.open("GET", "./data/events.json", false);
+   request.send(null)
+   var my_JSON_object = JSON.parse(request.responseText);
+   alert (my_JSON_object.result[0]);
