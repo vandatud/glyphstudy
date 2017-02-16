6108a523df2a50ea1a8b62ddbbc196374c0949b8
mkeck <mandy.keck@tu-dresden.de>
2017-02-16 15:16:26 +0100

    Aufgaben nochmal angepasst

--- js/configuration.js
+++ js/configuration.js
@@ -62,8 +62,8 @@ define(function() {
       "Finde die ähnlichste Veranstaltung zu der angegebenen aber mit einem günstigeren Preis!",
       "Finde die ähnlichste Veranstaltung zu der angegebenen aber mehr in deiner Nähe!",
       "Finde die ähnlichste Veranstaltung zu der angegebenen, die aber populärer ist!",
-      "Welche Veranstaltung hat die meisten niedrigsten Ausprägungen?",
-      "Welche Veranstaltung hat die meisten höchsten Ausprägungen?",
+      "Welche Veranstaltung hat in jeder Eigenschaft die niedrigsten Werte?",
+      "Welche Veranstaltung hat in jeder Eigenschaft die höchsten Werte?",
       "Zu welcher Kategorie gehören die meisten Veranstaltungen ohne Eintritt? Klicke eine Glyphe an, die dieser Kategorie entspricht!"
     ]
   };
